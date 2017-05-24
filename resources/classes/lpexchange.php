1682b9cfa0b28d69adcc001069d8b85ab6e876bd
Andrew Downes <mrdownes@hotmail.com>
2017-05-24 23:22:15 +0100

    add fallback cache

--- resources/classes/lpexchange.php
+++ resources/classes/lpexchange.php
@@ -9,6 +9,7 @@ class LPExchange {
 
   private $util;
   private $cache;
+  private $fallbackCache;
   private $workingCache;
   private $LPStore;
 
@@ -16,20 +17,33 @@ class LPExchange {
     $this->util = new Util();
     $this->cache = new FileCache('lpexchange_'.$corpId.'.json');
     $this->workingCache = new FileCache('lpexchange_'.$corpId.'_temp.json');
+    $this->fallbackCache = new FileCache('lpexchange_'.$corpId.'_fallback.json');
     $this->corpId = $corpId;
     $this->lpStore = new LPStore($corpId);
   }
 
   public function updateCache(){
     ini_set('max_execution_time', 300);
-    $workingCache = $this->workingCache->get();
-    if (!isset($workingCache->completed) || $workingCache->completed == true){
-      $workingCache = (object)[
-        'cachedUntil' => date('c', strtotime('+1 hour', time())),
-        'completed' => false,
-      ];
-    }
 
+    // If there is a working cache, the previous process broke part way so restart.
+    // If the working cache is corrupted/missing but a fallback cache exists, use that.
+    $workingCache = $this->workingCache->get();
+    $fallbackCache = $this->fallbackCache->get();
+    if (is_null($workingCache)){
+      if (is_null($fallbackCache)){
+        $workingCache = (object)[
+          'cachedUntil' => date('c', strtotime('+1 hour', time())),
+          'completed' => false,
+        ];
+      } 
+      else {
+        $workingCache = $fallbackCache;
+      }
+    }
+    $this->fallbackCache->set($workingCache);
+
+
+    $requiredItemsChecked = [];
     if (!isset($workingCache->lpStore)){
       $workingCache->lpStore = $this->lpStore->get()->lpStore;
       $this->workingCache->set($workingCache);
@@ -38,7 +52,10 @@ class LPExchange {
       if (!isset($item->highest_buy)){
         $this->updateLPStoreItemPrices($item);
         foreach ($item->required_items as $requiredIndex => $requiredItem) {
-          $this->updateLPStoreItemPrices($requiredItem);
+          if (!in_array($requiredItem->type_id, $requiredItemsChecked)) {
+            array_push($requiredItemsChecked, $requiredItem->type_id);
+            $this->updateLPStoreItemPrices($requiredItem);
+          }
         }
       }
     }
@@ -61,8 +78,9 @@ class LPExchange {
 
     // Caching completed.
     $workingCache->completed = true;
-    $this->workingCache->delete();
     $this->cache->set($workingCache);
+    $this->workingCache->delete();
+    $this->fallbackCache->delete();
   }
 
   public function get(){
