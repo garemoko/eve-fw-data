7052ca34d7603d5700c61c7a117ac99ad90f4ccf
Andrew Downes <mrdownes@hotmail.com>
2017-08-22 08:42:45 +0100

    start to add mark dashboard ui

--- market.php
+++ market.php
@@ -1,5 +1,5 @@
 <?php
-header("Content-type:application/json");
+header("Content-type:text/html");
 require_once( __DIR__ . "/resources/slack/classes/dashboardregistry.php");
 require_once( __DIR__ . "/resources/slack/classes/dashboard.php");
 
@@ -21,5 +21,34 @@ if (is_null($dashboardMetaData) || new DateTime($dashboardMetaData->expires) < n
 }
 
 $dashboard = new Dashboard($dashboardMetaData->slackToken, $dashboardMetaData->slackChannelId);
+$data = $dashboard->get();
 
-echo json_encode($dashboard->get(), JSON_PRETTY_PRINT);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Market Dashboard</title>
+    <link href="main.css" rel="stylesheet" />
+</head>
+<body>
+  <div class="title">
+    <h1>Market Dashboard</h1>
+    <p>By <a href="https://gate.eveonline.com/Profile/DrButterfly%20PHD" target="_blank">DrButterfly PHD</a></p>
+  </div>
+  <div class="links">
+    <b>Other Useful sites: </b>
+    <a href="https://evewarfare.com/" target="_blank">EVE Warfare</a>
+    | <a href="https://forums.eveonline.com/default.aspx?g=posts&t=496584" target="_blank">Ushra'Khan Recruitment</a> 
+    | <a href="http://evemaps.dotlan.net/" target="_blank">dotlan</a> 
+    | <a href="https://eve-central.com/" target="_blank">EVE Central</a>
+  </div><p class="times">&nbsp;</p>
+  <div class="content">
+  </div>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript">
+var data = <?php
+  echo json_encode($data, JSON_PRETTY_PRINT);
+?>;
+console.log(data);
+</script>
